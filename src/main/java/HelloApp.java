42d65f14aea389a5cf74f66cf4da07e4175b5750
ayushchoudhary30 <ayushcc08@gmail.com>
2026-03-17 13:20:08 +0530

    Coded UC1-UC5: Complete Hello App implementation

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,10 +1,21 @@
 /**
- * HelloApp - UC1: Display "Hello World"
+ * HelloApp - UC1-UC5: Progressive Hello App Implementation
+ * UC1: Display "Hello World" in the console
+ * UC2: Display "Hello" with Command-Line Argument
+ * UC3: Display "Hello" with Command-Line Argument or Default Message
+ * UC4: Display "Hello" with Multiple Command-Line Arguments or Default Message
+ * UC5: Display "Hello" with Multiple Command-Line Arguments using Enhanced For Loop
  * @author ayushchoudhary30
  * @version 1.0
  */
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
     }
 }
